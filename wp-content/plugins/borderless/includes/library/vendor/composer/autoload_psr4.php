6e3835a0ffa63c2f4ea3c63938cf99e8410f043c
MayankEnersys <mayank@enersys.co.th>
2022-12-19 08:18:02 +0530

    Add new Pages

--- wp-content/plugins/borderless/includes/library/vendor/composer/autoload_psr4.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$vendorDir = dirname(dirname(__FILE__));
-$baseDir = dirname($vendorDir);
-
-return array(
-    'LIBRARY\\' => array($baseDir . '/inc'),
-    'AwesomeMotive\\WPContentImporter2\\' => array($vendorDir . '/awesomemotive/wp-content-importer-v2/src'),
-);
