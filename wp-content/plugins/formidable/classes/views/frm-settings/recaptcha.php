3e7e31344c2d51b7b8b4508b659258762f3ed5b1
rashid <rashid@enersys.co.th>
2022-12-22 14:26:32 +0700

    Customized pages

--- wp-content/plugins/formidable/classes/views/frm-settings/recaptcha.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'You are not allowed to call this page directly.' );
-}
-
-_deprecated_file( esc_html( basename( __FILE__ ) ), 'x.x', 'formidable/classes/views/frm-settings/captcha/captcha.php' );
-
-include FrmAppHelper::plugin_path() . '/classes/views/frm-settings/captcha/captcha.php';
